1b7e266ef6c7dd27188fb0523bf5353fb5a57e97
Anim <Anim-101@users.noreply.github.com>
2019-04-14 18:01:08 +0600

    Update appEs6.js

--- Book List/appEs6.js
+++ Book List/appEs6.js
@@ -63,6 +63,15 @@ class UI {
         if(target.className === 'delete') {
 
             target.parentElement.parentElement.remove();
+
+            //Removing from LocalStorage
+            LocalStorage.remmoveBook(target.parentElement.previousElementSibling.textContent);
+
+            //Instantiating UI
+            const ui = new UI();
+
+            //Showing Alert
+            ui.showAlert('Book has been Removed!', 'success');
         }
     }
 
@@ -71,10 +80,72 @@ class UI {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
-
     }
 
 }
+
+//Local Storage Class
+class LocalStorage {
+
+    //Getting Books from Local Storage
+    static getBooks() {
+
+        let books;
+
+        if(localStorage.getItem('books') === null)
+        {
+            books = [];
+        }
+        else
+        {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    //Displaying Books from Local Storage
+    static displayBooks() {
+
+        const books = LocalStorage.getBooks();
+
+        books.forEach(function(book) {
+
+            const ui = new UI;
+
+            //Adding Books to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    //Adding Books in Local Storage
+    static addBook(book) {
+
+        const books = LocalStorage.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    //Removing Books from Local Storage
+    static remmoveBook(isbn) {
+ 
+        const books = LocalStorage.getBooks();
+
+        books.forEach(function(book, index) {
+
+            if(book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+//DOM Load Event Listeners
+document.addEventListener('DOMContentLoaded', LocalStorage.displayBooks);
 
 //Even Listener for Adding Books
 document.getElementById('book-form').addEventListener('submit', function(e) {
@@ -101,6 +172,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
         //Adding Book to List
         ui.addBookToList(book);
 
+        //Adding to Local Storage
+        LocalStorage.addBook(book);
+
         //Success Alert
         ui.showAlert('Book Information has been Added', 'success');
 
@@ -119,9 +193,6 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     //Deleting Books
     ui.deleteBook(e.target);
-
-    //Showing Alert
-    ui.showAlert('Book has been Removed!', 'success');
 
     e.preventDefault();
 });
